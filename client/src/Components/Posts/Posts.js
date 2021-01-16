87b9a139b2b3d8ef43ac694e4bd42bd8851c77de
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-16 10:16:10 +0200

    Post mapping to Posts.js and MUI components and icons added to Post.js

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 
-// Fetchin data from the global Redux store (selectors):
+// Fetching data from the global Redux store (selectors):
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
 import useStyles from './styles'
 
 const Posts = () => {
-
   const posts = useSelector((state) => state.posts);
-
   const classes = useStyles();
 
-  console.log(posts);  
-
   return (
-    <>
-    <h1>Posts</h1>
-    <Post/>
-    <Post/>
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className="classes.container" container alignItems="strech" spacing={3}>      
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post}/>
+          </Grid>
+        ))}
+      </Grid>    
+    )
   )
 }
 
 export default Posts;
+
+  // If no posts, show loading spinner, else show grid
+  // loop over the posts w map()  
+  // Grid: on small devices, post takes 12 / 12 spaces, in M/L 6 / 12 spaces
+  // Single post logic in Post.js
